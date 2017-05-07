653f52571b48cd9774e9efdc7b0dbd111e032ef4
dgeibi <png.inside@gmail.com>
2017-05-07 11:53:17 +0800

    add options: slugger, addID, parentLink, header, minLength

--- index.js
+++ index.js
@@ -3,13 +3,22 @@
 var cheerio = require('cheerio');
 var extend = require('extend-shallow');
 var slugify = require('markdown-slug');
+var escape = require('escape-html');
 
-module.exports = function(str, options) {
+module.exports = function (str, options) {
   if (typeof str !== 'string') {
     throw new TypeError('expected a string');
   }
 
-  var opts = extend({selectors: 'h1,h2', id: '#toc'}, options);
+  var opts = extend({
+    selectors: 'h1,h2',
+    id: '#toc',
+    slugger: slugify,
+    minLength: 0,
+    header: '',
+    addID: false,
+    parentLink: true
+  }, options);
   var $ = opts.$ || cheerio.load(str);
 
   // get all the anchor tags from inside the headers
@@ -23,7 +32,7 @@ module.exports = function(str, options) {
     }
     var loc = navigation[navigation.length - 1];
     if (!loc) {
-      loc = {children: []};
+      loc = { children: [] };
       navigation.push(loc);
     } else if (!loc.children) {
       loc.children = [];
@@ -31,12 +40,14 @@ module.exports = function(str, options) {
     return findLocation(loc.children, depth - 1);
   }
 
-  headings.map(function(i, ele) {
+
+
+  headings.map(function (i, ele) {
     var $ele = $(ele);
     var text = $ele.text().trim();
     if (!text) return;
 
-    var slug = slugify(text, {cache: slugs});
+    var slug = opts.slugger(text, { cache: slugs });
     var node = {
       text: text,
       link: slug,
@@ -53,19 +64,33 @@ module.exports = function(str, options) {
    */
 
   function buildHTML(navigation, first, sParentLink) {
-    return '<ul class="nav' + (first ? ' sidenav' : '') + '">' + navigation.map(function(loc) {
+    return '<ul class="nav' + (first ? ' sidenav' : '') + '">' + navigation.map(function (loc) {
 
       if (!loc || !loc.link) return '';
-      loc.link = (sParentLink ? sParentLink + '-' : '') + loc.link;
+      loc.link = (opts.parentLink && sParentLink ? sParentLink + '-' : '') + loc.link;
       loc.$ele.attr('id', loc.link);
 
       return '<li><a href="#' + loc.link + '">' + loc.text + '</a>' + (loc.children ? buildHTML(loc.children, false, loc.link) : '') + '</li>';
     }).join('\n') + '</ul>';
   }
 
-  $(opts.id).append(buildHTML(navigation, true));
+  function addID(navigation) {
+    navigation.forEach(function (loc) {
+      if (!loc || !loc.link) return;
+      loc.$ele.attr('id', loc.link);
+      if (loc.children) addID(loc.children);
+    });
+  }
 
-  headings.map(function(i, ele) {
+  if (headings.length < opts.minLength) {
+    if (opts.addID) addID(navigation);
+    else return $.html();
+  } else {
+    $(opts.id).append(opts.header);
+    $(opts.id).append(buildHTML(navigation, true));
+  }
+
+  headings.map(function (i, ele) {
     var $ele = $(ele);
     var id = $ele.attr('id');
 
